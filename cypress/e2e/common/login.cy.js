2bf3356fa0f6ca03d49c9145dd29656bf760dfd9
ornops <joshuaornopia@gmail.com>
2025-08-18 23:20:49 +0800

    Updated login specs

--- cypress/e2e/common/login.cy.js
+++ cypress/e2e/common/login.cy.js
@@ -5,8 +5,26 @@ describe('Login Test Suite', () => {
 	it('Valid Login Credentials', () => {
 
 		cy.visit('/');
-		cy.get('#user-name').type('joshtest');
+		cy.get('#user-name').type('standard_user');
+		cy.get('#password').type('secret_sauce');
 		cy.contains('Login').click();
 
+
 	})
+
+	it('Assert App Logo Title', () => {
+
+		cy.wait(3000);
+		cy.get('.app_logo').should('contain', 'Swag Labs');
+
+	})
+
+
+	// it('Save subject of the command', () => {
+
+	// 	cy.visit('/');
+	// 	cy.get('#user-name').type('joshtest');
+	// 	cy.contains('Login').click();
+
+	// })
 })
